e2c513b6c69eebea64ee447c2e6d5a103c1ca5b7
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2023-10-27 12:21:43 +0300

    Reloaded template for second lab

--- second_lab/src/main/java/com/k4r3l1ns/SecondLabApplication.java
+++ second_lab/src/main/java/com/k4r3l1ns/SecondLabApplication.java
@@ -1,25 +1,8 @@
 package com.k4r3l1ns;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class SecondLabApplication {
+
     public static void main(String[] args) {
-
-        Deque<Integer> stack = new ArrayDeque<>();
-
-        // <--
-        // 4 3 2 1 0
-        for (int i = 0; i < 5; ++i) {
-            stack.push(i);
-        }
-        stack.forEach(System.out::println);
-
-        // -->
-        // 2 1 0
-        for (int i = 0; i < 2; ++i) {
-            stack.pop();
-        }
-        stack.forEach(System.out::println);
+        System.out.println("Hello world!");
     }
 }
