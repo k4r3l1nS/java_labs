9ffb33990eaff0e71a313821ac867842f5b04efa
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2023-11-10 15:45:15 +0300

    Added parcel number support

--- second_lab/src/main/java/com/k4r3l1ns/service/ExpressionEvaluator.java
+++ second_lab/src/main/java/com/k4r3l1ns/service/ExpressionEvaluator.java
@@ -4,47 +4,64 @@ import com.k4r3l1ns.models.UnbalancedBracketsException;
 import com.k4r3l1ns.service.utils.BracketUtil;
 import com.k4r3l1ns.service.utils.FormConversionUtil;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
 
 public class ExpressionEvaluator {
+    public static final int DECIMAL_SCALE = 10;
 
-    public static Integer apply(String expression) {
+    public static BigDecimal apply(String expression) {
 
         try {
             BracketUtil.throwIfUnbalanced(expression);
+
             String postfix = FormConversionUtil.expressionToPostfix(expression);
 
-            Deque<Integer> stack = new ArrayDeque<>();
+            Deque<BigDecimal> stack = new ArrayDeque<>();
             Scanner scanner = new Scanner(postfix);
 
             while (scanner.hasNext()) {
-                if (scanner.hasNextInt()) {
-                    stack.push(scanner.nextInt());
+                if (scanner.hasNextDouble()) {
+                    stack.push(BigDecimal.valueOf(scanner.nextDouble()));
                 } else {
-                    int num2 = stack.pop();
-                    int num1 = stack.pop();
+                    BigDecimal secondNumber = stack.pop();
+                    BigDecimal firstNumber = stack.pop();
                     char op = scanner.next().charAt(0);
                     switch (op) {
                         case '+':
-                            stack.push(num1 + num2);
+                            stack.push(firstNumber.add(secondNumber));
                             break;
                         case '-':
-                            stack.push(num1 - num2);
+                            stack.push(firstNumber.subtract(secondNumber));
                             break;
                         case '*':
-                            stack.push(num1 * num2);
+                            stack.push(firstNumber.multiply(secondNumber));
                             break;
                         case '/':
-                            stack.push(num1 / num2);
+                            stack.push(
+                                    firstNumber.divide(
+                                            secondNumber,
+                                            DECIMAL_SCALE,
+                                            RoundingMode.HALF_UP
+                                    )
+                            );
                             break;
+                        case '^':
+                            stack.push(
+                                    BigDecimal.valueOf(
+                                            Math.pow(
+                                                    firstNumber.doubleValue(),
+                                                    secondNumber.doubleValue()
+                                            )
+                                    )
+                            );
                     }
                 }
             }
             scanner.close();
             return stack.pop();
-        } catch (UnbalancedBracketsException ex) {
+        } catch (UnbalancedBracketsException | ArithmeticException ex){
             ex.printStackTrace();
         }
         return null;
