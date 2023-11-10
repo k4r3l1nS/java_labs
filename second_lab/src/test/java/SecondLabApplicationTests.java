a6c4f3c2f880759d747167b6b6542298503225e0
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2023-11-10 23:32:39 +0300

    Added unit tests

---
+++ second_lab/src/test/java/SecondLabApplicationTests.java
@@ -0,0 +1,36 @@
+import com.k4r3l1ns.models.Expression;
+import com.k4r3l1ns.service.designation.VariableService;
+import com.k4r3l1ns.service.evaluator.ExpressionEvaluator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SecondLabApplicationTests {
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "abs(4 + sin[cos(3)]) + 2.3 * exp(neg(2.7))",   // ✓✓✓
+            "2 + [6 * (4 - 1])",                            // Скобки не сбалансированы
+            "7 + + 9",                                      // Кривое выражение
+            "43 / 0",                                       // Деление на 0
+            "abs(abs(abs(neg(5))))",                        // ✓✓✓
+            "neg(87) ^ 1",                                  // ✓✓✓
+            ""                                              // Выражения нет
+    }) public void testApplyingExpression(String mathText) {
+
+        Expression expression = new Expression(mathText);
+
+        // Альтернативный вариант - ввод с консоли
+        // Expression expression = new Expression();
+        // expression.scanExpression(DEFAULT_INPUT_STREAM);
+
+//        expression.scanVariables(System.in);
+
+        var result = ExpressionEvaluator.apply(expression, 3);
+        System.out.println(
+                "Результат вычислений: " + VariableService.replaceVariables(expression) +
+                        " = " + (result == null ? "-E-" : result) + "\n\n\n"
+        );
+    }
+
+}
